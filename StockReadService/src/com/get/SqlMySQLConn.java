42b63734f53d397a8cadfca0457de60eb834f1b2
fcadatastrategy <danjackson@dans-MacBook-Pro.local>
2016-11-06 09:18:10 +0000

    Initialise

---
+++ StockReadService/src/com/get/SqlMySQLConn.java
@@ -0,0 +1,56 @@
+package com.get;
+
+import java.sql.*;
+
+public class SqlMySQLConn {
+
+	private String url;
+	private String password;
+	private String username;
+	private static SqlMySQLConn instance;
+
+	private SqlMySQLConn()
+	{
+		try {
+			
+			System.out.println("C1");	
+			Class.forName("com.mysql.jdbc.Driver");  
+			url = "jdbc:mysql://localhost:3306/hokus";
+			username = "root";
+			password = "welcome1";
+			System.out.println("C2");
+
+		} catch (Exception e) {
+			System.out.println("Failed to Register JDBC Driver!");
+			e.printStackTrace();
+		}
+		System.out.println("Oracle JDBC Driver Registered!");
+	}
+
+	
+	public static Connection getConnection() throws SQLException {
+		if (instance == null) {
+			System.out.println("GC");
+			instance = new SqlMySQLConn();
+		}
+		try {
+			System.out.println("URL"+instance.url+instance.username);
+			return DriverManager.getConnection(instance.url,instance.username,instance.password);
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+	
+	public static void close(Connection connection)
+	{
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+	
---
+++ Stocker/src/com/stocker/rest/Stocks.java
@@ -0,0 +1,29 @@
+package com.stocker.rest;
+
+public class Stocks {
+
+	String StockId;
+	String StockName;
+	String StockExc;
+	public String getStockId() {
+		return StockId;
+	}
+	public void setStockId(String stockId) {
+		StockId = stockId;
+	}
+	public String getStockName() {
+		return StockName;
+	}
+	public void setStockName(String stockName) {
+		StockName = stockName;
+	}
+	public String getStockExc() {
+		return StockExc;
+	}
+	public void setStockExc(String stockExc) {
+		StockExc = stockExc;
+	}
+	
+	
+	
+}
